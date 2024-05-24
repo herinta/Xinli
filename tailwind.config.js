364046698a82ac1816660a49cf4b16b69997983c
Herinta A <herintya@gmail.com>
2024-05-24 16:32:03 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary':'#762BEB',
+      },
+      boxShadow: {
+        custom: '1px 1px 6px 6px rgba(0, 0, 0, 0.1)',
+      },
+      borderRadius: {
+        '50': '50px',
+      },
+      spacing: {
+        '61' : '248px'
+      },
+      fontWeight: {
+        'extra-light': 200,
+        'semi-bold': 500,
+        'extra-bold': 700,
+        'super-bold': 900
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 2s ease-in-out',
+      },
+    },
+  },
+  plugins: [
+    require('tailwindcss-animated')
+  ],
+}
+
